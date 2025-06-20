0f8aa1480ecec9c0dc43fe16c943e0eef449b2c8
Japa75 <charlesdelta76@gmail.com>
2025-06-20 11:36:49 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):  # <-- Herda UserMixin!
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
@@ -14,4 +14,13 @@ class User(db.Model, UserMixin):  # <-- Herda UserMixin!
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
 
